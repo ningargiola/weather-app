876718e5b226ddc4508d71d1006c9a67b33f1b0b
Nick Ingargiola <ningargiola1@gmail.com>
2024-07-31 02:21:13 -0400

    feat: bug fix

--- app/components/Mapbox/Mapbox.tsx
+++ app/components/Mapbox/Mapbox.tsx
@@ -1,66 +1,62 @@
 "use client";
-import React, { useEffect } from 'react'
-import { MapContainer, TileLayer, useMap } from 'react-leaflet';
-import 'leaflet/dist/leaflet.css';
-import { useGlobalContext } from '@/app/Context/globalContext';
+import React, { useEffect } from "react";
+import { MapContainer, TileLayer, useMap } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
+import { useGlobalContext } from "@/app/Context/globalContext";
 
 
-// @ts-ignore
-function FlyToActiveCity({activeCityCoords}) {
-    const map = useMap();
- 
+function FlyToActiveCity({ activeCityCords }) {
+  const map = useMap();
 
-    useEffect(() => {
-        if (activeCityCoords) {
-            const zoomLev = 13;
-            const flyToOptions = {
-                duration: 1.5,
-            };
+  useEffect(() => {
+    if (activeCityCords) {
+      const zoomLev = 13;
+      const flyToOptions = {
+        duration: 1.5,
+      };
 
-            map.flyTo(
-                [activeCityCoords.lat, activeCityCoords.lon], 
-                zoomLev, 
-                flyToOptions
-            );
-        }
-    }, [activeCityCoords, map]);
+      map.flyTo(
+        [activeCityCords.lat, activeCityCords.lon],
+        zoomLev,
+        flyToOptions
+      );
+    }
+  }, [activeCityCords, map]);
 
-    return null;
+  return null;
 }
 
 function Mapbox() {
-    const { forecast } = useGlobalContext();
+  const { forecast } = useGlobalContext(); // Your coordinates
 
-    const activeCityCords = forecast?.coord;
+  const activeCityCords = forecast?.coord;
 
-    if (!forecast || !forecast.coord || !activeCityCords) {
-        return (
-            <div>
-                <h1>Loading</h1>
-            </div>
-        );
-    }
-
-    
+  if (!forecast || !forecast.coord || !activeCityCords) {
+    return (
+      <div>
+        <h1>Loading</h1>
+      </div>
+    );
+  }
 
   return (
-    <div className='flex-1 basis-[50%] border rounded-lg'>
-        <MapContainer 
-            center={[activeCityCords.lat, activeCityCords.lon]} 
-            zoom={13} 
-            scrollWheelZoom={false}
-            className='rounded-lg m-4' 
-            style={{height: 'calc(100% - 2rem)', width: 'calc(100% - 2rem)'}}
-        >
-            <TileLayer
-                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-                attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-            />
+    <div className="flex-1 basis-[50%] border rounded-lg">
+      <MapContainer
+        center={[activeCityCords.lat, activeCityCords.lon]}
+        zoom={13}
+        scrollWheelZoom={false}
+        className="rounded-lg m-4"
+        style={{ height: "calc(100% - 2rem)", width: "calc(100% - 2rem)" }}
+      >
+        <TileLayer
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        />
 
-            <FlyToActiveCity activeCityCoords={activeCityCords} />
-        </MapContainer>
+        <FlyToActiveCity activeCityCords={activeCityCords} />
+      </MapContainer>
     </div>
-  )
+  );
 }
 
 export default Mapbox;
